c18826533497f96120150f620b7057b1491bbfbd
Göksel <can.onal@ozu.edu.tr>
2021-03-17 21:33:09 +0300

    Add files via upload

---
+++ LAB01/lab01_part1_max.hi.c
@@ -0,0 +1,5 @@
+//This program finds the maximum number among two number
+max = x;
+
+if(x < y)
+	max = y;
---
+++ LAB01/lab01_part2_max3_hi.c
@@ -0,0 +1,7 @@
+//This program finds the maximum number among three (a,b,c) numbers
+max = a;
+
+if(a < b)
+	max = b;
+if(max < c)
+	max = c;
